dc3cebb1e78d4409dcad1a567c5841160d8fc70e
user@unity.lxc <dev@d3d.info>
2019-03-12 22:27:52 +0000

    android build

---
+++ Assets/Scripts/EndUIHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EndUIHandler : MonoBehaviour, IPointerDownHandler
+{
+    private InputQueue inputQueue;
+
+    void Start()
+    {
+        inputQueue = GameController.GetCurrent().inputQueue;
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        inputQueue.TryToPutAction(InputAction.SkipStep);
+    }
+}
